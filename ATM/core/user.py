0b5e75de7b78e1163aa4b88c694ad21c90042bbb
sdau-kiven <306334678@qq.com>
2018-04-06 22:13:59 +0800

    modify

--- ATM/core/user.py
+++ ATM/core/user.py
@@ -8,29 +8,47 @@ from conf import setting
 logger_user = common.get_logger('user')
 
 
-# 通过用户名获取用户信息接口
 def get_userinfo_by_name(name):
+    '''
+    通过用户名获取用户信息接口
+    :param name:
+    :return:
+    '''
     return db_handle.select(name)
 
 
-# 锁定用户接口
 def lock_user(name):
+    '''
+    锁定用户接口
+    :param name:
+    :return:
+    '''
     user_dic = db_handle.select(name)
     user_dic['locked'] = True
     db_handle.update(user_dic)
     logger_user.info('%s 被锁定了' % name)
 
 
-# 解锁用户接口
 def unlock_user(name):
+    '''
+    解锁用户接口
+    :param name:
+    :return:
+    '''
     user_dic = get_userinfo_by_name(name)
     user_dic['locked'] = False
     db_handle.update(user_dic)
     logger_user.info('%s 被解锁了' % name)
 
 
-# 注册用户接口
 def register_user(name, password, banlance=15000):
+    '''
+    注册用户接口
+    :param name:
+    :param password:
+    :param banlance:
+    :return:
+    '''
     user_dic = {'name': name, 'password': password, 'locked': False, 'account': banlance, 'credit': banlance,
                 'shopping_cart': {}, 'bankflow': []}
     db_handle.update(user_dic)
